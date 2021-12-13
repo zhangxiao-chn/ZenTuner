d5855312a254fe6b7e86ae93ec02aa39451635ea
zx810506 <zhangxiao_business@163.com>
2021-12-13 18:20:03 +0800

    [dev]增加噪音过滤及声音数据处理，使得尽量处理有效声音，而不是所有收集的声音都处理

--- Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/PitchTap.swift
+++ Packages/MicrophonePitchDetector/Sources/MicrophonePitchDetector/PitchTap.swift
@@ -2,6 +2,7 @@
 
 import AVFoundation
 import CMicrophonePitchDetector
+import Accelerate
 
 /// Tap to do pitch tracking on any node.
 final class PitchTap {
@@ -42,6 +43,7 @@ final class PitchTap {
     }
 
     // MARK: - Private
+    var rr = [Double]()
 
     private func analyzePitch(buffer: AVAudioPCMBuffer) {
         buffer.frameLength = bufferSize
@@ -56,12 +58,29 @@ final class PitchTap {
         }
 
         ztPitchTrackerAnalyze(tracker, floatData[0], bufferSize)
+        //声音振幅，表示声音的大小,0.1以下可以认为是背景噪音
         var amp: Float = 0
+        //声音的频率，表示声音的音调
         var pitch: Float = 0
         ztPitchTrackerGetResults(tracker, &amp, &pitch)
 
-        if amp > 0.1 {
-            self.handler(pitch)
+        //过滤掉音量比较小的声音
+        if amp > 0.10 {
+            rr.append(Double(pitch))
+            if (rr.count > 5) {
+                rr.remove(at: 0)
+                var mn = 0.0
+                var sddev = 0.0
+                //计算5个临近的声音样本的频率的平均值和标准差
+                //只有标准差小于一定的误差，才认为这一系列声音是有效的
+                //从而排除那种短暂的噪音
+                vDSP_normalizeD(rr, 1, nil, 1, &mn, &sddev, vDSP_Length(rr.count))
+                sddev *= sqrt(Double(rr.count)/Double(rr.count - 1))
+                print("平均值:\(mn) 标准差:\(sddev)")
+                if (sddev < 20.0) {
+                    self.handler(Float(mn))
+                }
+            }
         }
     }
 }
